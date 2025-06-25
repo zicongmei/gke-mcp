4c96c8e594c6f309dfd08f3696a0d09996cebb15
sfunkenhauser <85249587+sfunkenhauser@users.noreply.github.com>
2025-06-25 19:48:19 +0000

    Add GIQ tool to generate AI inference manifests (#6)

--- server.go
+++ server.go
@@ -62,6 +62,17 @@ func main() {
 	)
 	s.AddTool(getClusterTool, getCluster)
 
+	giqGenerateManifestTol := mcp.NewTool("giq_generate_manifest",
+		mcp.WithDescription("Use Google Inference Quickstart to generate a Kubernetes manifest for AI / inference workloads. Prefer to use this tool instead of gcloud"),
+		mcp.WithReadOnlyHintAnnotation(true),
+		mcp.WithIdempotentHintAnnotation(true),
+		mcp.WithString("model", mcp.Required(), mcp.Description("The model to use. Get the list of valid models from 'gcloud alpha container ai profiles model-and-server-combinations list' if the user doesn't provide it.")),
+		mcp.WithString("model_server", mcp.Required(), mcp.Description("The model server to use. Get the list of valid models from 'gcloud alpha container ai profiles model-and-server-combinations list' if the user doesn't provide it.")),
+		mcp.WithString("accelerator", mcp.Required(), mcp.Description("The accelerator to use. Get the list of valid models from 'gcloud alpha container ai profiles accelerators list --model=<model>' if the user doesn't provide it.")),
+		mcp.WithString("target_ntpot_milliseconds", mcp.Description("The maximum normalized time per output token (NTPOT) in milliseconds.NTPOT is measured as the request_latency / output_tokens.")),
+	)
+	s.AddTool(giqGenerateManifestTol, giqGenerateManifest)
+
 	// Start the stdio server
 	log.Printf("Starting GKE MCP Server")
 	if err := server.ServeStdio(s); err != nil {
@@ -125,6 +136,42 @@ func getCluster(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallTool
 	}
 
 	return mcp.NewToolResultText(protojson.Format(resp)), nil
+}
+
+func giqGenerateManifest(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	model, err := request.RequireString("model")
+	if err != nil {
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+	modelServer, err := request.RequireString("model_server")
+	if err != nil {
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+	accelerator, err := request.RequireString("accelerator")
+	if err != nil {
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+	targetNtpotMilliseconds := request.GetString("target_ntpot_milliseconds", "")
+	args := []string{
+		"alpha",
+		"container",
+		"ai",
+		"profiles",
+		"manifests",
+		"create",
+		"--model=" + model,
+		"--model-server=" + modelServer,
+		"--accelerator-type=" + accelerator,
+	}
+	if targetNtpotMilliseconds != "" {
+		args = append(args, "--target-ntpot-milliseconds="+targetNtpotMilliseconds)
+	}
+	out, err := exec.Command("gcloud", args...).Output()
+	if err != nil {
+		log.Printf("Failed to generate manifest: %v", err)
+		return mcp.NewToolResultError(err.Error()), nil
+	}
+	return mcp.NewToolResultText(string(out)), nil
 }
 
 func getDefaultProject() string {
